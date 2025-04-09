72521fcb80eebf2513601a3b91bdd99727e4f30a
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-04-09 23:37:01 +0600

    Updated User data

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ con.connect((err)=>{
     if(!err){
         console.log('Connection Success');
         //InsertData(con);
-         deleteData(con);
+        //deleteData(con);
+        UpdateData(con);
     }
     else{
         console.log('connection fail');
@@ -28,7 +29,7 @@ con.connect((err)=>{
 //Insert Data Fucnction
 function InsertData(con){
 
-    let SQLQuery = "INSERT INTO `student_list`(`name`, `roll`, `class`, `phone`, `city`) VALUES ('Kabirul','011202026','BSC','017********','Mymensingh')"
+    let SQLQuery = "INSERT INTO `student_list`(`name`, `roll`, `class`, `phone`, `city`) VALUES ('Rakin','011202006','BSC','017********','Mymensingh')"
     con.query(SQLQuery, function(err){
         if(!err){
             console.log("Data Insert Successfully");
@@ -46,6 +47,19 @@ const deleteData= (con)=>{
             console.log('Data Deleted Successfully');
         }else{
             console.log("Data not deleted");
+        }
+    })
+}
+
+//Update Data
+
+const UpdateData = (con) =>{
+    let SQLQuery = "UPDATE `student_list` SET `name`='Md. Kabirul Hossain' WHERE `id`=1"
+    con.query(SQLQuery, (err)=>{
+        if(!err){
+            console.log('Update Data Successfully!');
+        }else{
+            console.log("Data Not Updated!!");
         }
     })
 }
