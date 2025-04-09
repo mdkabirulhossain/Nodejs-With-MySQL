b452c36dbb8c20c871c78a9cacdbe2695923aa01
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-04-09 23:08:57 +0600

    Dete data from database successfully

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const con = mysql.createConnection(DatabaseConnectionConfig);
 con.connect((err)=>{
     if(!err){
         console.log('Connection Success');
-        InsertData(con);
+        //InsertData(con);
+         deleteData(con);
     }
     else{
         console.log('connection fail');
@@ -33,6 +34,18 @@ function InsertData(con){
             console.log("Data Insert Successfully");
         }else{
             console.log("Data Insert Fail");
+        }
+    })
+}
+
+// delete Data
+const deleteData= (con)=>{
+    let SQLQuery = "DELETE FROM `student_list` WHERE `id`= 2";
+    con.query(SQLQuery, (err)=>{
+        if(!err){
+            console.log('Data Deleted Successfully');
+        }else{
+            console.log("Data not deleted");
         }
     })
 }
