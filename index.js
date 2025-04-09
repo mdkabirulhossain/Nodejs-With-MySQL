56011574970c33c52b744b667acba9ec257690cb
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-04-09 22:04:26 +0600

    Insert data in MySQL Database

--- index.js
+++ index.js
@@ -5,19 +5,34 @@ const DatabaseConnectionConfig = {
     host:"localhost",
     user:"root",
     password:"",
-    database:"demo",
+    database:"school",
 }
 
 
-const connection = mysql.createConnection(DatabaseConnectionConfig);
+const con = mysql.createConnection(DatabaseConnectionConfig);
 
 
 //connection
-connection.connect((err)=>{
+con.connect((err)=>{
     if(!err){
         console.log('Connection Success');
+        InsertData(con);
     }
     else{
         console.log('connection fail');
+        
     }
 })
+
+//Insert Data Fucnction
+function InsertData(con){
+
+    let SQLQuery = "INSERT INTO `student_list`(`name`, `roll`, `class`, `phone`, `city`) VALUES ('Kabirul','011202026','BSC','017********','Mymensingh')"
+    con.query(SQLQuery, function(err){
+        if(!err){
+            console.log("Data Insert Successfully");
+        }else{
+            console.log("Data Insert Fail");
+        }
+    })
+}
